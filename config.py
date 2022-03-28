a78e9528024e57479a55dffa31cae69113ece165
심현섭 <89609620+SHIMspacethere@users.noreply.github.com>
2022-03-29 00:09:16 +0900

    test

---
+++ config.py
@@ -0,0 +1,4 @@
+WEBHOOK_PASSPHRASE = "yourpassphrase"
+
+API_KEY = '27xy5KpWGTmNd0SWPSlpnCdVJdbQNnNctOB8q1TybixtJTVJb2XAuQ4kB23oDuKy'
+API_SECRET = 'ntnbSDtBxPfvMZrspcQJVwr6oqa1eWa4dOs25pA99Ow88RSqakHmsWkPR0tf12FM'
